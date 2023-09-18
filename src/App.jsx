e6cd7530f9f0f620c888eeda5ebcab06d59492fe
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2023-09-18 13:33:44 +0600

    refactor navbar component and create a reusable link component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar></Navbar>
       <h1 className=" text-5xl">Vite + React</h1>
     </>
   );
